f06c5a4f21dd9b62396733d02da98ee1ddbdb372
DzmitryGrom <Shelegeykodmitriy@mail.ru>
2016-11-15 23:28:01 +0200

    add content for search-work page

--- js/search-work.js
+++ js/search-work.js
@@ -2,13 +2,11 @@
 $(document).ready(function () {
     'use strict';
     $(".btn-open").on("click", function() {
-        $(this).next().toggleClass("hidden");
-        if (!$('.content-search-list-item__filter-slider-panel').hasClass("hidden")) {
-            $(this).empty();
+        if ($('.filter-item-slider-panel').hasClass("hidden")) {
             $(this).html("<i class='fa fa-angle-up'></i>Дополнительно");
         } else {
-            $(this).empty();
             $(this).html("<i class='fa fa-angle-down'></i>Дополнительно"); 
         }
+        $(this).next().toggleClass("hidden");
     });
 });
--- js/slider-filter-search.js
+++ js/slider-filter-search.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     'use strict';
 
-    var sliderTwo, sliderSecond;
+    var sliderTwo, sliderThree, sliderSecond;
     
     
     /// filter
@@ -10,7 +10,7 @@ $(document).ready(function () {
     sliderTwo = jQuery("#Slider4").slider({
         from: 0,
         to: 100000,
-        scale: ['0', '500', '1000', '', '', '', '', '', '', '', '', '', '', '100000'],
+        scale: ['0', '500', '1000', '', '5000', '', '', '10000', '', '25000', '', '50000', '', '&#8734'],
         limits: false,
         step: 1,
         skin: "round_plastic"
@@ -18,6 +18,18 @@ $(document).ready(function () {
     
 
     sliderSecond = sliderTwo.slider();
+    
+    sliderThree = jQuery("#Slider3").slider({
+        from: 0,
+        to: 35,
+        scale: ['0', '', '', '', '', '', '', '7', '', '', '', '10', '', '', '',  '14', '', '', '', '', '', '', '21', '', '', '', '', '', '', '28', '', '', '31', '&#8734'],
+        limits: false,
+        step: 1,
+        skin: "round_plastic"
+    });
+    
+
+    sliderSecond = sliderThree.slider();
     
 
 });
